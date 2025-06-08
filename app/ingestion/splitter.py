0f859123c0348642352deddb26c81c148b90133f
Ayush Sharma <sharmaayushmvn@gmail.com>
2025-06-08 10:46:04 +0530

    updated changes

--- app/ingestion/splitter.py
+++ app/ingestion/splitter.py
@@ -1,12 +1,6 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema import Document
 
-# def split_text(text):
-#     splitter = RecursiveCharacterTextSplitter(
-#         chunk_size=1000,
-#         chunk_overlap=100
-#     )
-#     return splitter.create_documents([text])
 
 def split_text(text, metadata=None):
     splitter = RecursiveCharacterTextSplitter(
